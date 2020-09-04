8ca3ba459faa352db44ccd11483b4a1b6c41a59b
Bolton Bailey <bolton.bailey@gmail.com>
2020-09-04 15:00:46 -0700

    rewrote patriciaLookup.add()

--- patriciaaccumulator/forest.go
+++ patriciaaccumulator/forest.go
@@ -295,65 +295,59 @@ func (t *PatriciaLookup) add(stateRoot Hash, location uint64, toAdd Hash) (Hash,
 	let mut neighbor: H256;
 	let mut sibling: PatriciaNode;
 
-	loop {
-		if !node.common.in_range(&location) {
-			// The target location is not in range
-			// This proves the location is not in the state.
-			// We replace node with the combination of node and the new leaf
-			node_to_add = PatriciaNode::new(&new_leaf_node, &node);
-			self.tree_nodes
-				.insert(node_to_add.hash(), node_to_add.clone());
-			while branch.len() > 0 {
-				// We travel up the branch, recombining nodes
-				sibling = branch.pop().unwrap();
-				node_to_add = PatriciaNode::new(&sibling, &node_to_add);
-				self.tree_nodes
-					.insert(node_to_add.hash(), node_to_add.clone());
-			}
-			return MerkleRoot {
-				root: node_to_add.hash(),
-			};
+	var hash Hash
+	var neighbor Node
+	neighborBranch := new([]PatriciaNode, 0, 64)
+	mainBranch := new([]PatriciaNode, 0, 64)
+
+	// We descend the tree until we find a node which does not contain the location at which we are adding
+	// This node should be the sibling of the new leaf
+	for node.inRange(location) {
+		// mainBranch will consist of all the nodes to be replaced, from the root to the parent of the new sibling of the new leaf
+		mainBranch = append(mainBranch, node)
+		// If the min is the max, we are at a preexisting leaf
+		// THIS SHOULD NOT HAPPEN. We never add on top of something already added in this code
+		// (Block validity should already be checked at this point)
+		if node.left == node.right {
+			panic()
 		}
-		// If the min is the max, we have the whole hash so we are at the leaf,
-		if node.common.min == node.common.max {
-			// We must check if the hash is in the lookup table
-			if !self.account_data.contains_key(&node.left) {
-				debug!("ERROR: lookup not constructed properly; missing AccountState");
-				assert!(false);
-			} else {
-				// let prospective_account: AccountState = self.account_data[&node.left].clone();
-				// If we are adding a node, then we are overwriting the previous node.
-				node_to_add = new_leaf_node;
-				while branch.len() > 0 {
-					// We travel up the branch, recombining nodes
-					sibling = branch.pop().unwrap();
-					node_to_add = PatriciaNode::new(&sibling, &node_to_add);
-					self.tree_nodes
-						.insert(node_to_add.hash(), node_to_add.clone());
-				}
-				return MerkleRoot {
-					root: node_to_add.hash(),
-				};
-			}
-		}
-		// We are not yet at the leaf, so we descend the tree.
+
 		// Determine if we descend left or right
-		if node.common.in_left(&location) {
-			hash = node.left;
-			neighbor = node.right;
+		if node.inLeft(location) {
+			neighbor = t.treeNodes[node.right]
+			hash = node.left
 		} else {
-			hash = node.right;
-			neighbor = node.left;
-		}
-		if !self.tree_nodes.contains_key(&hash) || !self.tree_nodes.contains_key(&neighbor) {
-			debug!("ERROR: lookup not constructed properly; missing PatriciaNode");
-			assert!(false);
+			neighbor = t.treeNodes[node.left]
+			hash = node.right
 		}
 		// Add the other direction node to the branch
-		branch.push(self.tree_nodes[&neighbor].clone());
+		neighborBranch = append(neighborBranch, neighbor)
 		// Update node down the tree
-		node = self.tree_nodes[&hash].clone();
+		node = t.treeNodes[hash]
+
 	} // End loop
+
+	// Add the new leaf node
+	newLeafNode := PatriciaNode{toAdd, toAdd, location}
+	t.treeNodes[newLeafNode.hash()] = newLeafNode
+	// Combine leaf node with its new sibling
+	nodeToAdd = newPatriciaNode(newLeafNode, node)
+	t.treeNodes[nodeToAdd.hash()] = nodeToAdd
+
+	// We travel up the branch, recombining nodes
+	for len(branch) > 0 {
+		// nodeToAdd must now replace the hash of the last node of mainBranch in the second-to-last node of mainBranch
+		neighborNode = neighborBranch.pop()
+		nodeToAdd = newPatriciaNode(neighborNode, nodeToAdd)
+		t.treeNodes[nodeToAdd.hash()] = nodeToAdd
+	}
+	// Delete all nodes in the main branch, they have been replaced
+	for _, node := range mainBranch {
+		delete(t.treeNodes, node.hash())
+	}
+
+	// The new state root is the hash of the last node added
+	return nodeToAdd.hash()
 }
 
 // Delete the leaves at the dels location for the trie forest
