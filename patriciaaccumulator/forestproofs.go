aeee3d5b9aafba9b204db081068784d9191477ca
Bolton Bailey <bolton.bailey@gmail.com>
2020-09-20 12:26:07 -0700

    commented out pollard code we haven't changed it yet

--- patriciaaccumulator/forestproofs.go
+++ patriciaaccumulator/forestproofs.go
@@ -129,143 +129,143 @@ func (f *Forest) VerifyMany(ps []Proof) bool {
 	return true
 }
 
-// ProveBatch gets proofs (in the form of a node slice) for a bunch of leaves
-// The ordering of Targets is the same as the ordering of hashes given as
-// argument.
-// NOTE However targets will need to be sorted before using the proof!
-// TODO the elements to be proven should not be included in the proof.
-func (f *Forest) ProveBatch(hs []Hash) (BatchProof, error) {
-	starttime := time.Now()
-	var bp BatchProof
-	// skip everything if empty (should this be an error?
-	if len(hs) == 0 {
-		return bp, nil
-	}
-	if f.data.size() < 2 {
-		return bp, nil
-	}
+// // ProveBatch gets proofs (in the form of a node slice) for a bunch of leaves
+// // The ordering of Targets is the same as the ordering of hashes given as
+// // argument.
+// // NOTE However targets will need to be sorted before using the proof!
+// // TODO the elements to be proven should not be included in the proof.
+// func (f *Forest) ProveBatch(hs []Hash) (BatchProof, error) {
+// 	starttime := time.Now()
+// 	var bp BatchProof
+// 	// skip everything if empty (should this be an error?
+// 	if len(hs) == 0 {
+// 		return bp, nil
+// 	}
+// 	if f.data.size() < 2 {
+// 		return bp, nil
+// 	}
 
-	// first get all the leaf positions
-	// there shouldn't be any duplicates in hs, but if there are I guess
-	// it's not an error.
-	bp.Targets = make([]uint64, len(hs))
+// 	// first get all the leaf positions
+// 	// there shouldn't be any duplicates in hs, but if there are I guess
+// 	// it's not an error.
+// 	bp.Targets = make([]uint64, len(hs))
 
-	for i, wanted := range hs {
+// 	for i, wanted := range hs {
 
-		pos, ok := f.positionMap[wanted.Mini()]
-		if !ok {
-			fmt.Print(f.ToString())
-			return bp, fmt.Errorf("hash %x not found", wanted)
-		}
+// 		pos, ok := f.positionMap[wanted.Mini()]
+// 		if !ok {
+// 			fmt.Print(f.ToString())
+// 			return bp, fmt.Errorf("hash %x not found", wanted)
+// 		}
 
-		// should never happen
-		if pos > f.numLeaves {
-			for m, p := range f.positionMap {
-				fmt.Printf("%x @%d\t", m[:4], p)
-			}
-			return bp, fmt.Errorf(
-				"ProveBatch: got leaf position %d but only %d leaves exist",
-				pos, f.numLeaves)
-		}
-		bp.Targets[i] = pos
-	}
-	// targets need to be sorted because the proof hashes are sorted
-	// NOTE that this is a big deal -- we lose in-block positional information
-	// because of this sorting.  Does that hurt locality or performance?  My
-	// guess is no, but that's untested.
-	sortedTargets := make([]uint64, len(bp.Targets))
-	copy(sortedTargets, bp.Targets)
-	sortUint64s(sortedTargets)
+// 		// should never happen
+// 		if pos > f.numLeaves {
+// 			for m, p := range f.positionMap {
+// 				fmt.Printf("%x @%d\t", m[:4], p)
+// 			}
+// 			return bp, fmt.Errorf(
+// 				"ProveBatch: got leaf position %d but only %d leaves exist",
+// 				pos, f.numLeaves)
+// 		}
+// 		bp.Targets[i] = pos
+// 	}
+// 	// targets need to be sorted because the proof hashes are sorted
+// 	// NOTE that this is a big deal -- we lose in-block positional information
+// 	// because of this sorting.  Does that hurt locality or performance?  My
+// 	// guess is no, but that's untested.
+// 	sortedTargets := make([]uint64, len(bp.Targets))
+// 	copy(sortedTargets, bp.Targets)
+// 	sortUint64s(sortedTargets)
 
-	// TODO feels like you could do all this with just slices and no maps...
-	// that would be better
-	// proofTree is the partially populated tree of everything needed for the
-	// proofs
-	proofTree := make(map[uint64]Hash)
+// 	// TODO feels like you could do all this with just slices and no maps...
+// 	// that would be better
+// 	// proofTree is the partially populated tree of everything needed for the
+// 	// proofs
+// 	proofTree := make(map[uint64]Hash)
 
-	// go through each target and add a proof for it up to the intersection
-	for _, pos := range sortedTargets {
-		// add hash for the deletion itself and its sibling
-		// if they already exist, skip the whole thing
-		_, alreadyThere := proofTree[pos]
-		if alreadyThere {
-			//			fmt.Printf("%d omit already there\n", pos)
-			continue
-		}
-		// TODO change this for the real thing; no need to prove 0-tree root.
-		// but we still need to verify it and tag it as a target.
-		if pos == f.numLeaves-1 && pos&1 == 0 {
-			proofTree[pos] = f.data.read(pos)
-			// fmt.Printf("%d add as root\n", pos)
-			continue
-		}
+// 	// go through each target and add a proof for it up to the intersection
+// 	for _, pos := range sortedTargets {
+// 		// add hash for the deletion itself and its sibling
+// 		// if they already exist, skip the whole thing
+// 		_, alreadyThere := proofTree[pos]
+// 		if alreadyThere {
+// 			//			fmt.Printf("%d omit already there\n", pos)
+// 			continue
+// 		}
+// 		// TODO change this for the real thing; no need to prove 0-tree root.
+// 		// but we still need to verify it and tag it as a target.
+// 		if pos == f.numLeaves-1 && pos&1 == 0 {
+// 			proofTree[pos] = f.data.read(pos)
+// 			// fmt.Printf("%d add as root\n", pos)
+// 			continue
+// 		}
 
-		// always put in both siblings when on the bottom row
-		// this can be out of order but it will be sorted later
-		proofTree[pos] = f.data.read(pos)
-		proofTree[pos^1] = f.data.read(pos ^ 1)
-		// fmt.Printf("added leaves %d, %d\n", pos, pos^1)
+// 		// always put in both siblings when on the bottom row
+// 		// this can be out of order but it will be sorted later
+// 		proofTree[pos] = f.data.read(pos)
+// 		proofTree[pos^1] = f.data.read(pos ^ 1)
+// 		// fmt.Printf("added leaves %d, %d\n", pos, pos^1)
 
-		treeTop := detectSubTreeRows(pos, f.numLeaves, f.rows)
-		pos = parent(pos, f.rows)
-		// go bottom to top and add siblings into the partial tree
-		// start at row 1 though; we always populate the bottom leaf and sibling
-		// This either gets to the top, or intersects before that and deletes
-		// something
-		for h := uint8(1); h < treeTop; h++ {
-			// check if the sibling is already there, in which case we're done
-			// also check if the parent itself is there, in which case we delete it!
-			// I think this with the early ignore at the bottom make it optimal
-			_, selfThere := proofTree[pos]
-			_, sibThere := proofTree[pos^1]
-			if sibThere {
-				// sibling position already exists in partial tree; done
-				// with this branch
+// 		treeTop := detectSubTreeRows(pos, f.numLeaves, f.rows)
+// 		pos = parent(pos, f.rows)
+// 		// go bottom to top and add siblings into the partial tree
+// 		// start at row 1 though; we always populate the bottom leaf and sibling
+// 		// This either gets to the top, or intersects before that and deletes
+// 		// something
+// 		for h := uint8(1); h < treeTop; h++ {
+// 			// check if the sibling is already there, in which case we're done
+// 			// also check if the parent itself is there, in which case we delete it!
+// 			// I think this with the early ignore at the bottom make it optimal
+// 			_, selfThere := proofTree[pos]
+// 			_, sibThere := proofTree[pos^1]
+// 			if sibThere {
+// 				// sibling position already exists in partial tree; done
+// 				// with this branch
 
-				// TODO seems that this never happens and can be removed
-				panic("this never happens...?")
-			}
-			if selfThere {
-				// self position already there; remove as children are known
-				//				fmt.Printf("remove proof from pos %d\n", pos)
+// 				// TODO seems that this never happens and can be removed
+// 				panic("this never happens...?")
+// 			}
+// 			if selfThere {
+// 				// self position already there; remove as children are known
+// 				//				fmt.Printf("remove proof from pos %d\n", pos)
 
-				delete(proofTree, pos)
-				delete(proofTree, pos^1) // right? can delete both..?
-				break
-			}
-			// fmt.Printf("add proof from pos %d\n", pos^1)
-			proofTree[pos^1] = f.data.read(pos ^ 1)
-			pos = parent(pos, f.rows)
-		}
-	}
+// 				delete(proofTree, pos)
+// 				delete(proofTree, pos^1) // right? can delete both..?
+// 				break
+// 			}
+// 			// fmt.Printf("add proof from pos %d\n", pos^1)
+// 			proofTree[pos^1] = f.data.read(pos ^ 1)
+// 			pos = parent(pos, f.rows)
+// 		}
+// 	}
 
-	var nodeSlice []node
+// 	var nodeSlice []node
 
-	// run through partial tree to turn it into a slice
-	for pos, hash := range proofTree {
-		nodeSlice = append(nodeSlice, node{pos, hash})
-	}
-	// fmt.Printf("made nodeSlice %d nodes\n", len(nodeSlice))
+// 	// run through partial tree to turn it into a slice
+// 	for pos, hash := range proofTree {
+// 		nodeSlice = append(nodeSlice, node{pos, hash})
+// 	}
+// 	// fmt.Printf("made nodeSlice %d nodes\n", len(nodeSlice))
 
-	// sort the slice of nodes (even though we only want the hashes)
-	sortNodeSlice(nodeSlice)
-	// copy the sorted / in-order hashes into a hash slice
-	bp.Proof = make([]Hash, len(nodeSlice))
+// 	// sort the slice of nodes (even though we only want the hashes)
+// 	sortNodeSlice(nodeSlice)
+// 	// copy the sorted / in-order hashes into a hash slice
+// 	bp.Proof = make([]Hash, len(nodeSlice))
 
-	for i, n := range nodeSlice {
-		bp.Proof[i] = n.Val
-	}
-	if verbose {
-		fmt.Printf("blockproof targets: %v\n", bp.Targets)
-	}
+// 	for i, n := range nodeSlice {
+// 		bp.Proof[i] = n.Val
+// 	}
+// 	if verbose {
+// 		fmt.Printf("blockproof targets: %v\n", bp.Targets)
+// 	}
 
-	donetime := time.Now()
-	f.TimeInProve += donetime.Sub(starttime)
-	return bp, nil
-}
+// 	donetime := time.Now()
+// 	f.TimeInProve += donetime.Sub(starttime)
+// 	return bp, nil
+// }
 
-// VerifyBatchProof :
-func (f *Forest) VerifyBatchProof(bp BatchProof) bool {
-	ok, _ := verifyBatchProof(bp, f.getRoots(), f.numLeaves, f.rows)
-	return ok
-}
+// // VerifyBatchProof :
+// func (f *Forest) VerifyBatchProof(bp BatchProof) bool {
+// 	ok, _ := verifyBatchProof(bp, f.getRoots(), f.numLeaves, f.rows)
+// 	return ok
+// }
--- patriciaaccumulator/pollarfull.go
+++ patriciaaccumulator/pollarfull.go
@@ -37,135 +37,135 @@ func (p *Pollard) PosMapSanity() error {
 
 // TODO make interface to reduce code dupe
 
-// ProveBatch but for pollard.
-// Now getting really obvious that forest and pollard should both satisfy some
-// kind of utreexo-like interface.  And maybe forest shouldn't be called forest.
-// Anyway do that after this.
-func (p *Pollard) ProveBatch(hs []Hash) (BatchProof, error) {
-	var bp BatchProof
-	// skip everything if empty (should this be an error?
-	if len(hs) == 0 {
-		return bp, nil
-	}
-	if p.numLeaves < 2 {
-		return bp, nil
-	}
+// // ProveBatch but for pollard.
+// // Now getting really obvious that forest and pollard should both satisfy some
+// // kind of utreexo-like interface.  And maybe forest shouldn't be called forest.
+// // Anyway do that after this.
+// func (p *Pollard) ProveBatch(hs []Hash) (BatchProof, error) {
+// 	var bp BatchProof
+// 	// skip everything if empty (should this be an error?
+// 	if len(hs) == 0 {
+// 		return bp, nil
+// 	}
+// 	if p.numLeaves < 2 {
+// 		return bp, nil
+// 	}
 
-	// for h, p := range f.positionMap {
-	// 	fmt.Printf("%x@%d ", h[:4], p)
-	// }
+// 	// for h, p := range f.positionMap {
+// 	// 	fmt.Printf("%x@%d ", h[:4], p)
+// 	// }
 
-	// first get all the leaf positions
-	// there shouldn't be any duplicates in hs, but if there are I guess
-	// it's not an error.
-	bp.Targets = make([]uint64, len(hs))
+// 	// first get all the leaf positions
+// 	// there shouldn't be any duplicates in hs, but if there are I guess
+// 	// it's not an error.
+// 	bp.Targets = make([]uint64, len(hs))
 
-	for i, wanted := range hs {
+// 	for i, wanted := range hs {
 
-		pos, ok := p.positionMap[wanted.Mini()]
-		if !ok {
-			fmt.Print(p.ToString())
-			return bp, fmt.Errorf("hash %x not found", wanted)
-		}
+// 		pos, ok := p.positionMap[wanted.Mini()]
+// 		if !ok {
+// 			fmt.Print(p.ToString())
+// 			return bp, fmt.Errorf("hash %x not found", wanted)
+// 		}
 
-		// should never happen
-		if pos > p.numLeaves {
-			for m, p := range p.positionMap {
-				fmt.Printf("%x @%d\t", m[:4], p)
-			}
-			return bp, fmt.Errorf(
-				"ProveBlock: got leaf position %d but only %d leaves exist",
-				pos, p.numLeaves)
-		}
-		bp.Targets[i] = pos
-	}
-	// targets need to be sorted because the proof hashes are sorted
-	// NOTE that this is a big deal -- we lose in-block positional information
-	// because of this sorting.  Does that hurt locality or performance?  My
-	// guess is no, but that's untested.
-	sortUint64s(bp.Targets)
+// 		// should never happen
+// 		if pos > p.numLeaves {
+// 			for m, p := range p.positionMap {
+// 				fmt.Printf("%x @%d\t", m[:4], p)
+// 			}
+// 			return bp, fmt.Errorf(
+// 				"ProveBlock: got leaf position %d but only %d leaves exist",
+// 				pos, p.numLeaves)
+// 		}
+// 		bp.Targets[i] = pos
+// 	}
+// 	// targets need to be sorted because the proof hashes are sorted
+// 	// NOTE that this is a big deal -- we lose in-block positional information
+// 	// because of this sorting.  Does that hurt locality or performance?  My
+// 	// guess is no, but that's untested.
+// 	sortUint64s(bp.Targets)
 
-	// TODO feels like you could do all this with just slices and no maps...
-	// that would be better
-	// proofTree is the partially populated tree of everything needed for the
-	// proofs
-	proofTree := make(map[uint64]Hash)
+// 	// TODO feels like you could do all this with just slices and no maps...
+// 	// that would be better
+// 	// proofTree is the partially populated tree of everything needed for the
+// 	// proofs
+// 	proofTree := make(map[uint64]Hash)
 
-	// go through each target and add a proof for it up to the intersection
-	for _, pos := range bp.Targets {
-		// add hash for the deletion itself and its sibling
-		// if they already exist, skip the whole thing
-		_, alreadyThere := proofTree[pos]
-		if alreadyThere {
-			//			fmt.Printf("%d omit already there\n", pos)
-			continue
-		}
-		// TODO change this for the real thing; no need to prove 0-tree root.
-		// but we still need to verify it and tag it as a target.
-		if pos == p.numLeaves-1 && pos&1 == 0 {
-			proofTree[pos] = p.read(pos)
-			// fmt.Printf("%d add as root\n", pos)
-			continue
-		}
+// 	// go through each target and add a proof for it up to the intersection
+// 	for _, pos := range bp.Targets {
+// 		// add hash for the deletion itself and its sibling
+// 		// if they already exist, skip the whole thing
+// 		_, alreadyThere := proofTree[pos]
+// 		if alreadyThere {
+// 			//			fmt.Printf("%d omit already there\n", pos)
+// 			continue
+// 		}
+// 		// TODO change this for the real thing; no need to prove 0-tree root.
+// 		// but we still need to verify it and tag it as a target.
+// 		if pos == p.numLeaves-1 && pos&1 == 0 {
+// 			proofTree[pos] = p.read(pos)
+// 			// fmt.Printf("%d add as root\n", pos)
+// 			continue
+// 		}
 
-		// always put in both siblings when on the bottom row
-		// this can be out of order but it will be sorted later
-		proofTree[pos] = p.read(pos)
-		proofTree[pos^1] = p.read(pos ^ 1)
-		// fmt.Printf("added leaves %d, %d\n", pos, pos^1)
+// 		// always put in both siblings when on the bottom row
+// 		// this can be out of order but it will be sorted later
+// 		proofTree[pos] = p.read(pos)
+// 		proofTree[pos^1] = p.read(pos ^ 1)
+// 		// fmt.Printf("added leaves %d, %d\n", pos, pos^1)
 
-		treeRoot := detectSubTreeRows(pos, p.numLeaves, p.rows())
-		pos = parent(pos, p.rows())
-		// go bottom to top and add siblings into the partial tree
-		// start at row 1 though; we always populate the bottom leaf and sibling
-		// This either gets to the top, or intersects before that and deletes
-		// something
-		for r := uint8(1); r < treeRoot; r++ {
-			// check if the sibling is already there, in which case we're done
-			// also check if the parent itself is there, in which case we delete it!
-			// I think this with the early ignore at the bottom make it optimal
-			_, selfThere := proofTree[pos]
-			_, sibThere := proofTree[pos^1]
-			if sibThere {
-				// sibling position already exists in partial tree; done
-				// with this branch
+// 		treeRoot := detectSubTreeRows(pos, p.numLeaves, p.rows())
+// 		pos = parent(pos, p.rows())
+// 		// go bottom to top and add siblings into the partial tree
+// 		// start at row 1 though; we always populate the bottom leaf and sibling
+// 		// This either gets to the top, or intersects before that and deletes
+// 		// something
+// 		for r := uint8(1); r < treeRoot; r++ {
+// 			// check if the sibling is already there, in which case we're done
+// 			// also check if the parent itself is there, in which case we delete it!
+// 			// I think this with the early ignore at the bottom make it optimal
+// 			_, selfThere := proofTree[pos]
+// 			_, sibThere := proofTree[pos^1]
+// 			if sibThere {
+// 				// sibling position already exists in partial tree; done
+// 				// with this branch
 
-				// TODO seems that this never happens and can be removed
-				panic("this never happens...?")
-			}
-			if selfThere {
-				// self position already there; remove as children are known
-				//				fmt.Printf("remove proof from pos %d\n", pos)
+// 				// TODO seems that this never happens and can be removed
+// 				panic("this never happens...?")
+// 			}
+// 			if selfThere {
+// 				// self position already there; remove as children are known
+// 				//				fmt.Printf("remove proof from pos %d\n", pos)
 
-				delete(proofTree, pos)
-				delete(proofTree, pos^1) // right? can delete both..?
-				break
-			}
-			// fmt.Printf("add proof from pos %d\n", pos^1)
-			proofTree[pos^1] = p.read(pos ^ 1)
-			pos = parent(pos, p.rows())
-		}
-	}
+// 				delete(proofTree, pos)
+// 				delete(proofTree, pos^1) // right? can delete both..?
+// 				break
+// 			}
+// 			// fmt.Printf("add proof from pos %d\n", pos^1)
+// 			proofTree[pos^1] = p.read(pos ^ 1)
+// 			pos = parent(pos, p.rows())
+// 		}
+// 	}
 
-	var nodeSlice []node
+// 	var nodeSlice []node
 
-	// run through partial tree to turn it into a slice
-	for pos, hash := range proofTree {
-		nodeSlice = append(nodeSlice, node{pos, hash})
-	}
-	// fmt.Printf("made nodeSlice %d nodes\n", len(nodeSlice))
+// 	// run through partial tree to turn it into a slice
+// 	for pos, hash := range proofTree {
+// 		nodeSlice = append(nodeSlice, node{pos, hash})
+// 	}
+// 	// fmt.Printf("made nodeSlice %d nodes\n", len(nodeSlice))
 
-	// sort the slice of nodes (even though we only want the hashes)
-	sortNodeSlice(nodeSlice)
-	// copy the sorted / in-order hashes into a hash slice
-	bp.Proof = make([]Hash, len(nodeSlice))
+// 	// sort the slice of nodes (even though we only want the hashes)
+// 	sortNodeSlice(nodeSlice)
+// 	// copy the sorted / in-order hashes into a hash slice
+// 	bp.Proof = make([]Hash, len(nodeSlice))
 
-	for i, n := range nodeSlice {
-		bp.Proof[i] = n.Val
-	}
-	if verbose {
-		fmt.Printf("blockproof targets: %v\n", bp.Targets)
-	}
+// 	for i, n := range nodeSlice {
+// 		bp.Proof[i] = n.Val
+// 	}
+// 	if verbose {
+// 		fmt.Printf("blockproof targets: %v\n", bp.Targets)
+// 	}
 
-	return bp, nil
-}
+// 	return bp, nil
+// }
