0f0d1423d9a8a922b39f3332a6d4425fa1f3dee6
Bolton Bailey <bolton.bailey@gmail.com>
2020-09-26 21:22:16 -0700

    commented out more tests

--- patriciaaccumulator/pollardfull_test.go
+++ patriciaaccumulator/pollardfull_test.go
@@ -1,102 +1,96 @@
 package patriciaaccumulator
 
-import (
-	"fmt"
-	"math/rand"
-	"testing"
-)
+// func TestPollardFullRand(t *testing.T) {
+// 	for z := 0; z < 30; z++ {
+// 		// z := 1
+// 		rand.Seed(int64(z))
+// 		fmt.Printf("randseed %d\n", z)
+// 		err := pollardFullRandomRemember(20)
+// 		if err != nil {
+// 			fmt.Printf("randseed %d\n", z)
+// 			t.Fatal(err)
+// 		}
+// 	}
+// }
 
-func TestPollardFullRand(t *testing.T) {
-	for z := 0; z < 30; z++ {
-		// z := 1
-		rand.Seed(int64(z))
-		fmt.Printf("randseed %d\n", z)
-		err := pollardFullRandomRemember(20)
-		if err != nil {
-			fmt.Printf("randseed %d\n", z)
-			t.Fatal(err)
-		}
-	}
-}
+// func pollardFullRandomRemember(blocks int32) error {
 
-func pollardFullRandomRemember(blocks int32) error {
+// 	// ffile, err := os.Create("/dev/shm/forfile")
+// 	// if err != nil {
+// 	// return err
+// 	// }
 
-	// ffile, err := os.Create("/dev/shm/forfile")
-	// if err != nil {
-	// return err
-	// }
+// 	var fp, p Pollard
+// 	fp = NewFullPollard()
 
-	var fp, p Pollard
-	fp = NewFullPollard()
+// 	// p.Minleaves = 0
 
-	// p.Minleaves = 0
+// 	sn := NewSimChain(0x07)
+// 	sn.lookahead = 400
+// 	for b := int32(0); b < blocks; b++ {
+// 		adds, _, delHashes := sn.NextBlock(rand.Uint32() & 0x03)
 
-	sn := NewSimChain(0x07)
-	sn.lookahead = 400
-	for b := int32(0); b < blocks; b++ {
-		adds, _, delHashes := sn.NextBlock(rand.Uint32() & 0x03)
+// 		fmt.Printf("\t\t\tstart block %d del %d add %d - %s\n",
+// 			sn.blockHeight, len(delHashes), len(adds), p.Stats())
 
-		fmt.Printf("\t\t\tstart block %d del %d add %d - %s\n",
-			sn.blockHeight, len(delHashes), len(adds), p.Stats())
+// 		// get proof for these deletions (with respect to prev block)
+// 		bp, err := fp.ProveBatch(delHashes)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		// get proof for these deletions (with respect to prev block)
-		bp, err := fp.ProveBatch(delHashes)
-		if err != nil {
-			return err
-		}
+// 		// verify proofs on rad node
+// 		err = p.IngestBatchProof(bp)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		fmt.Printf("del %v\n", bp.Targets)
 
-		// verify proofs on rad node
-		err = p.IngestBatchProof(bp)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("del %v\n", bp.Targets)
+// 		// apply adds and deletes to the bridge node (could do this whenever)
+// 		err = fp.Modify(adds, bp.Targets)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		// TODO fix: there is a leak in forest.Modify where sometimes
+// 		// the position map doesn't clear out and a hash that doesn't exist
+// 		// any more will be stuck in the positionMap.  Wastes a bit of memory
+// 		// and seems to happen when there are moves to and from a location
+// 		// Should fix but can leave it for now.
 
-		// apply adds and deletes to the bridge node (could do this whenever)
-		err = fp.Modify(adds, bp.Targets)
-		if err != nil {
-			return err
-		}
-		// TODO fix: there is a leak in forest.Modify where sometimes
-		// the position map doesn't clear out and a hash that doesn't exist
-		// any more will be stuck in the positionMap.  Wastes a bit of memory
-		// and seems to happen when there are moves to and from a location
-		// Should fix but can leave it for now.
+// 		err = fp.PosMapSanity()
+// 		if err != nil {
+// 			fmt.Print(fp.ToString())
+// 			return err
+// 		}
 
-		err = fp.PosMapSanity()
-		if err != nil {
-			fmt.Print(fp.ToString())
-			return err
-		}
+// 		// apply adds / dels to pollard
+// 		err = p.Modify(adds, bp.Targets)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		// apply adds / dels to pollard
-		err = p.Modify(adds, bp.Targets)
-		if err != nil {
-			return err
-		}
+// 		fmt.Printf("pol postadd %s", p.ToString())
 
-		fmt.Printf("pol postadd %s", p.ToString())
+// 		fmt.Printf("fulpol postadd %s", fp.ToString())
 
-		fmt.Printf("fulpol postadd %s", fp.ToString())
+// 		fullTops := fp.rootHashesReverse()
+// 		polTops := p.rootHashesReverse()
 
-		fullTops := fp.rootHashesReverse()
-		polTops := p.rootHashesReverse()
+// 		// check that tops match
+// 		if len(fullTops) != len(polTops) {
+// 			return fmt.Errorf("block %d fulpol %d tops, pol %d tops",
+// 				sn.blockHeight, len(fullTops), len(polTops))
+// 		}
+// 		fmt.Printf("top matching: ")
+// 		for i, ft := range fullTops {
+// 			fmt.Printf("fp %04x p %04x ", ft[:4], polTops[i][:4])
+// 			if ft != polTops[i] {
+// 				return fmt.Errorf("block %d top %d mismatch, fulpol %x pol %x",
+// 					sn.blockHeight, i, ft[:4], polTops[i][:4])
+// 			}
+// 		}
+// 		fmt.Printf("\n")
+// 	}
 
-		// check that tops match
-		if len(fullTops) != len(polTops) {
-			return fmt.Errorf("block %d fulpol %d tops, pol %d tops",
-				sn.blockHeight, len(fullTops), len(polTops))
-		}
-		fmt.Printf("top matching: ")
-		for i, ft := range fullTops {
-			fmt.Printf("fp %04x p %04x ", ft[:4], polTops[i][:4])
-			if ft != polTops[i] {
-				return fmt.Errorf("block %d top %d mismatch, fulpol %x pol %x",
-					sn.blockHeight, i, ft[:4], polTops[i][:4])
-			}
-		}
-		fmt.Printf("\n")
-	}
-
-	return nil
-}
+// 	return nil
+// }
--- patriciaaccumulator/undo_test.go
+++ patriciaaccumulator/undo_test.go
@@ -1,172 +1,166 @@
 package patriciaaccumulator
 
-import (
-	"fmt"
-	"math/rand"
-	"testing"
-)
+// func TestUndoFixed(t *testing.T) {
+// 	rand.Seed(2)
 
-func TestUndoFixed(t *testing.T) {
-	rand.Seed(2)
+// 	// needs in-order
+// 	err := undoAddDelOnce(6, 4, 4)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
-	// needs in-order
-	err := undoAddDelOnce(6, 4, 4)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// func TestUndoRandom(t *testing.T) {
 
-func TestUndoRandom(t *testing.T) {
+// 	for z := int64(0); z < 100; z++ {
+// 		// z := int64(11)
+// 		rand.Seed(z)
+// 		err := undoOnceRandom(20)
+// 		if err != nil {
+// 			fmt.Printf("rand seed %d\n", z)
+// 			t.Fatal(err)
+// 		}
+// 	}
+// }
 
-	for z := int64(0); z < 100; z++ {
-		// z := int64(11)
-		rand.Seed(z)
-		err := undoOnceRandom(20)
-		if err != nil {
-			fmt.Printf("rand seed %d\n", z)
-			t.Fatal(err)
-		}
-	}
-}
+// func TestUndoTest(t *testing.T) {
+// 	rand.Seed(1)
+// 	err := undoTestSimChain()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
-func TestUndoTest(t *testing.T) {
-	rand.Seed(1)
-	err := undoTestSimChain()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// func undoOnceRandom(blocks int32) error {
+// 	f := NewForest(nil, false)
 
-func undoOnceRandom(blocks int32) error {
-	f := NewForest(nil, false)
+// 	sc := NewSimChain(0x07)
+// 	sc.lookahead = 0
+// 	for b := int32(0); b < blocks; b++ {
 
-	sc := NewSimChain(0x07)
-	sc.lookahead = 0
-	for b := int32(0); b < blocks; b++ {
+// 		adds, durations, delHashes := sc.NextBlock(rand.Uint32() & 0x03)
 
-		adds, durations, delHashes := sc.NextBlock(rand.Uint32() & 0x03)
+// 		fmt.Printf("\t\tblock %d del %d add %d - %s\n",
+// 			sc.blockHeight, len(delHashes), len(adds), f.Stats())
 
-		fmt.Printf("\t\tblock %d del %d add %d - %s\n",
-			sc.blockHeight, len(delHashes), len(adds), f.Stats())
+// 		bp, err := f.ProveBatch(delHashes)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		bp.SortTargets()
+// 		ub, err := f.Modify(adds, bp.Targets)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		fmt.Print(f.ToString())
+// 		fmt.Print(sc.ttlString())
+// 		for h, p := range f.positionMap {
+// 			fmt.Printf("%x@%d ", h[:4], p)
+// 		}
+// 		err = f.PosMapSanity()
+// 		if err != nil {
+// 			return err
+// 		}
 
-		bp, err := f.ProveBatch(delHashes)
-		if err != nil {
-			return err
-		}
-		bp.SortTargets()
-		ub, err := f.Modify(adds, bp.Targets)
-		if err != nil {
-			return err
-		}
-		fmt.Print(f.ToString())
-		fmt.Print(sc.ttlString())
-		for h, p := range f.positionMap {
-			fmt.Printf("%x@%d ", h[:4], p)
-		}
-		err = f.PosMapSanity()
-		if err != nil {
-			return err
-		}
+// 		//undo every 3rd block
+// 		if b%3 == 2 {
+// 			fmt.Print(ub.ToString())
+// 			err := f.Undo(*ub)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			fmt.Print("\n post undo map: ")
+// 			for h, p := range f.positionMap {
+// 				fmt.Printf("%x@%d ", h[:4], p)
+// 			}
+// 			sc.BackOne(adds, durations, delHashes)
+// 		}
 
-		//undo every 3rd block
-		if b%3 == 2 {
-			fmt.Print(ub.ToString())
-			err := f.Undo(*ub)
-			if err != nil {
-				return err
-			}
-			fmt.Print("\n post undo map: ")
-			for h, p := range f.positionMap {
-				fmt.Printf("%x@%d ", h[:4], p)
-			}
-			sc.BackOne(adds, durations, delHashes)
-		}
+// 	}
+// 	return nil
+// }
 
-	}
-	return nil
-}
+// func undoAddDelOnce(numStart, numAdds, numDels uint32) error {
+// 	f := NewForest(nil, false)
+// 	sc := NewSimChain(0xff)
 
-func undoAddDelOnce(numStart, numAdds, numDels uint32) error {
-	f := NewForest(nil, false)
-	sc := NewSimChain(0xff)
+// 	// --------------- block 0
+// 	// make starting forest with numStart leaves, and store tops
+// 	adds, _, _ := sc.NextBlock(numStart)
+// 	fmt.Printf("adding %d leaves\n", numStart)
+// 	_, err := f.Modify(adds, nil)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	fmt.Printf(f.ToString())
+// 	beforeTops := f.getRoots()
+// 	for i, h := range beforeTops {
+// 		fmt.Printf("beforeTops %d %x\n", i, h)
+// 	}
 
-	// --------------- block 0
-	// make starting forest with numStart leaves, and store tops
-	adds, _, _ := sc.NextBlock(numStart)
-	fmt.Printf("adding %d leaves\n", numStart)
-	_, err := f.Modify(adds, nil)
-	if err != nil {
-		return err
-	}
-	fmt.Printf(f.ToString())
-	beforeTops := f.getRoots()
-	for i, h := range beforeTops {
-		fmt.Printf("beforeTops %d %x\n", i, h)
-	}
+// 	// ---------------- block 1
+// 	// just delete from the left side for now.  Can try deleting scattered
+// 	// randomly later
+// 	delHashes := make([]Hash, numDels)
+// 	for i, _ := range delHashes {
+// 		delHashes[i] = adds[i].Hash
+// 	}
+// 	// get some more adds; the dels we already got
+// 	adds, _, _ = sc.NextBlock(numAdds)
 
-	// ---------------- block 1
-	// just delete from the left side for now.  Can try deleting scattered
-	// randomly later
-	delHashes := make([]Hash, numDels)
-	for i, _ := range delHashes {
-		delHashes[i] = adds[i].Hash
-	}
-	// get some more adds; the dels we already got
-	adds, _, _ = sc.NextBlock(numAdds)
+// 	bp, err := f.ProveBatch(delHashes)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	bp, err := f.ProveBatch(delHashes)
-	if err != nil {
-		return err
-	}
+// 	fmt.Printf("block 1 add %d rem %d\n", numAdds, numDels)
 
-	fmt.Printf("block 1 add %d rem %d\n", numAdds, numDels)
+// 	ub, err := f.Modify(adds, bp.Targets)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	fmt.Print(f.ToString())
+// 	fmt.Print(ub.ToString())
+// 	afterTops := f.getRoots()
+// 	for i, h := range afterTops {
+// 		fmt.Printf("afterTops %d %x\n", i, h)
+// 	}
 
-	ub, err := f.Modify(adds, bp.Targets)
-	if err != nil {
-		return err
-	}
-	fmt.Print(f.ToString())
-	fmt.Print(ub.ToString())
-	afterTops := f.getRoots()
-	for i, h := range afterTops {
-		fmt.Printf("afterTops %d %x\n", i, h)
-	}
+// 	err = f.Undo(*ub)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	err = f.Undo(*ub)
-	if err != nil {
-		return err
-	}
+// 	undoneTops := f.getRoots()
+// 	for i, h := range undoneTops {
+// 		fmt.Printf("undoneTops %d %x\n", i, h)
+// 	}
+// 	for h, p := range f.positionMap {
+// 		fmt.Printf("%x@%d ", h[:4], p)
+// 	}
+// 	fmt.Printf("tops: ")
+// 	for i, _ := range beforeTops {
+// 		fmt.Printf("pre %04x post %04x ", beforeTops[i][:4], undoneTops[i][:4])
+// 		if undoneTops[i] != beforeTops[i] {
+// 			return fmt.Errorf("block %d top %d mismatch, pre %x post %x",
+// 				sc.blockHeight, i, beforeTops[i][:4], undoneTops[i][:4])
+// 		}
+// 	}
+// 	fmt.Printf("\n")
 
-	undoneTops := f.getRoots()
-	for i, h := range undoneTops {
-		fmt.Printf("undoneTops %d %x\n", i, h)
-	}
-	for h, p := range f.positionMap {
-		fmt.Printf("%x@%d ", h[:4], p)
-	}
-	fmt.Printf("tops: ")
-	for i, _ := range beforeTops {
-		fmt.Printf("pre %04x post %04x ", beforeTops[i][:4], undoneTops[i][:4])
-		if undoneTops[i] != beforeTops[i] {
-			return fmt.Errorf("block %d top %d mismatch, pre %x post %x",
-				sc.blockHeight, i, beforeTops[i][:4], undoneTops[i][:4])
-		}
-	}
-	fmt.Printf("\n")
+// 	return nil
+// }
 
-	return nil
-}
+// func undoTestSimChain() error {
 
-func undoTestSimChain() error {
-
-	sc := NewSimChain(7)
-	sc.NextBlock(3)
-	sc.NextBlock(3)
-	sc.NextBlock(3)
-	fmt.Printf(sc.ttlString())
-	l1, dur, h1 := sc.NextBlock(3)
-	fmt.Printf(sc.ttlString())
-	sc.BackOne(l1, dur, h1)
-	fmt.Printf(sc.ttlString())
-	return nil
-}
+// 	sc := NewSimChain(7)
+// 	sc.NextBlock(3)
+// 	sc.NextBlock(3)
+// 	sc.NextBlock(3)
+// 	fmt.Printf(sc.ttlString())
+// 	l1, dur, h1 := sc.NextBlock(3)
+// 	fmt.Printf(sc.ttlString())
+// 	sc.BackOne(l1, dur, h1)
+// 	fmt.Printf(sc.ttlString())
+// 	return nil
+// }
