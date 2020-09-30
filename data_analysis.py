3d27ded4bc8dfeb05437003b7dd27acba304ba05
Bolton Bailey <bolton.bailey@gmail.com>
2020-09-30 16:47:02 -0700

    adding data and analysis file

---
+++ data_analysis.py
@@ -0,0 +1,34 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+with open('patriciasizedata.csv', newline='') as csvfile:
+    our_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+    our_data = [int(d[1]) for d in our_data]
+
+
+with open('utreexosizedata.csv', newline='') as csvfile:
+    utreexo_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+    utreexo_data = [int(d[1]) for d in utreexo_data]
+
+
+# assert len(our_data) == len(utreexo_data)
+
+SMOOTH = 1000
+
+our_data_smoothed = [sum(our_data[i:i+SMOOTH])/SMOOTH for i in range(len(our_data) - SMOOTH)]
+
+utreexo_data_smoothed = [sum(utreexo_data[i:i+SMOOTH])/SMOOTH for i in range(len(utreexo_data) - SMOOTH)]
+
+print(our_data)
+
+plt.plot(range(len(our_data_smoothed)), our_data_smoothed, label="Ours")
+plt.plot(range(len(utreexo_data_smoothed)), utreexo_data_smoothed, label="Utreexo")
+plt.legend()
+plt.xlabel("Block #")
+plt.ylabel("Bytes per block (smoothed)")
+
+plt.show()
