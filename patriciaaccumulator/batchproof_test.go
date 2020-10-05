25af6da01a970d6b950abddd6ea5ee2692236586
Bolton Bailey <bolton.bailey@gmail.com>
2020-10-05 14:02:46 -0700

    uncommented some tests

--- patriciaaccumulator/batchproof_test.go
+++ patriciaaccumulator/batchproof_test.go
@@ -1,61 +1,63 @@
 package patriciaaccumulator
 
-// // TestVerifyBlockProof tests that the computedTop is compared to the top in the
-// // Utreexo forest.
-// func TestVerifyBatchProof(t *testing.T) {
-// 	// Create forest in memory
-// 	f := NewForest(nil, false)
+import "testing"
 
-// 	// last index to be deleted. Same as blockDels
-// 	lastIdx := uint64(7)
+// TestVerifyBlockProof tests that the computedTop is compared to the top in the
+// Utreexo forest.
+func TestVerifyBatchProof(t *testing.T) {
+	// Create forest in memory
+	f := NewForest(nil, false)
 
-// 	// Generate adds
-// 	adds := make([]Leaf, 8)
-// 	adds[0].Hash = Hash{1}
-// 	adds[1].Hash = Hash{2}
-// 	adds[2].Hash = Hash{3}
-// 	adds[3].Hash = Hash{4}
-// 	adds[4].Hash = Hash{5}
-// 	adds[5].Hash = Hash{6}
-// 	adds[6].Hash = Hash{7}
-// 	adds[7].Hash = Hash{8}
+	// last index to be deleted. Same as blockDels
+	lastIdx := uint64(7)
 
-// 	// Modify with the additions to simulate txos being added
-// 	_, err := f.Modify(adds, nil)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	// Generate adds
+	adds := make([]Leaf, 8)
+	adds[0].Hash = Hash{1}
+	adds[1].Hash = Hash{2}
+	adds[2].Hash = Hash{3}
+	adds[3].Hash = Hash{4}
+	adds[4].Hash = Hash{5}
+	adds[5].Hash = Hash{6}
+	adds[6].Hash = Hash{7}
+	adds[7].Hash = Hash{8}
 
-// 	// create blockProof based on the last add in the slice
-// 	blockProof, err := f.ProveBatch(
-// 		[]Hash{adds[lastIdx].Hash})
+	// Modify with the additions to simulate txos being added
+	_, err := f.Modify(adds, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	// create blockProof based on the last add in the slice
+	blockProof, err := f.ProveBatch(
+		[]Hash{adds[lastIdx].Hash})
 
-// 	// Confirm that verify block proof works
-// 	shouldBetrue := f.VerifyBatchProof(blockProof)
-// 	if shouldBetrue != true {
-// 		t.Fail()
-// 		t.Logf("Block failed to verify")
-// 	}
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	// delete last leaf and add a new leaf
-// 	adds = make([]Leaf, 1)
-// 	adds[0].Hash = Hash{9}
-// 	_, err = f.Modify(adds, []uint64{lastIdx})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	// Confirm that verify block proof works
+	shouldBetrue := f.VerifyBatchProof(blockProof)
+	if shouldBetrue != true {
+		t.Fail()
+		t.Logf("Block failed to verify")
+	}
 
-// 	// Attempt to verify block proof with deleted element
-// 	shouldBeFalse := f.VerifyBatchProof(blockProof)
-// 	if shouldBeFalse != false {
-// 		t.Fail()
-// 		t.Logf("Block verified with old proof. Double spending allowed.")
-// 	}
-// }
+	// delete last leaf and add a new leaf
+	adds = make([]Leaf, 1)
+	adds[0].Hash = Hash{9}
+	_, err = f.Modify(adds, []uint64{lastIdx})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Attempt to verify block proof with deleted element
+	shouldBeFalse := f.VerifyBatchProof(blockProof)
+	if shouldBeFalse != false {
+		t.Fail()
+		t.Logf("Block verified with old proof. Double spending allowed.")
+	}
+}
 
 // // In a two leaf tree:
 // // We prove one node, then delete the other one.
