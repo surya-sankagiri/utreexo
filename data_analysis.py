a00b9d3d9b14d09be1f621fc6da8becfe2286343
Bolton Bailey <bolton.bailey@gmail.com>
2021-01-16 15:38:45 -0800

    update

--- data_analysis.py
+++ data_analysis.py
@@ -3,19 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with open('patriciasizedata.csv', newline='') as csvfile:
-    our_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+with open('patriciasizedatacomplete.csv', newline='') as csvfile:
+    our_data = csv.DictReader(csvfile, delimiter=',', quotechar='|')
 
-    our_data = [int(d[1]) for d in our_data]
+    our_data = [int(d["zlib"]) for d in our_data]
 
 
-with open('utreexosizedata.csv', newline='') as csvfile:
-    utreexo_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+with open('utreexosizedatacomplete.csv', newline='') as csvfile:
+    utreexo_data = csv.DictReader(csvfile, delimiter=',', quotechar='|')
 
-    utreexo_data = [int(d[1]) for d in utreexo_data]
+    utreexo_data = [int(d["zlib"]) for d in utreexo_data]
 
 
 min_data_len = min(len(utreexo_data), len(our_data))
+# min_data_len = 500000
 
 utreexo_data = utreexo_data[:min_data_len]
 our_data = our_data[:min_data_len]
@@ -23,19 +24,24 @@ our_data = our_data[:min_data_len]
 print(len(our_data), len(utreexo_data))
 assert len(our_data) == len(utreexo_data)
 
+
 SMOOTH = 1000
 
-our_data_smoothed = [sum(our_data[i:i+SMOOTH])/SMOOTH for i in range(len(our_data) - SMOOTH)]
+our_data_smoothed = [sum(our_data[i:i+SMOOTH])/(1000*SMOOTH) for i in range(len(our_data) - SMOOTH)]
 
-utreexo_data_smoothed = [sum(utreexo_data[i:i+SMOOTH])/SMOOTH for i in range(len(utreexo_data) - SMOOTH)]
+utreexo_data_smoothed = [sum(utreexo_data[i:i+SMOOTH])/(1000*SMOOTH) for i in range(len(utreexo_data) - SMOOTH)]
 
-print(sum(our_data)/sum(utreexo_data))
+print("ours", sum(our_data))
+print("theirs", sum(utreexo_data))
+print("ratio", sum(our_data)/sum(utreexo_data))
 
-
+plt.figure(figsize=(10, 6))
 plt.plot(range(len(our_data_smoothed)), our_data_smoothed, label="Ours")
-plt.plot(range(len(utreexo_data_smoothed)), utreexo_data_smoothed, label="Utreexo")
+plt.plot(range(len(utreexo_data_smoothed)), utreexo_data_smoothed, label="UTREEXO")
 plt.legend()
 plt.xlabel("Block #")
-plt.ylabel("Bytes per block (smoothed)")
+plt.ylabel("Kilobytes per block (smoothed)")
+plt.tight_layout()
 
-plt.show()
+plt.savefig('plot.pdf')
+# plt.show()
